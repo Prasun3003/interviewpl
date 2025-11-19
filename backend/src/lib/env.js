c8542dd2f88c700057bc8bea0dedbede7ddd4cb5
Prasun3003 <prasunjha3003@gmail.com>
2025-11-19 21:57:24 +0530

    deployment config added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -4,4 +4,5 @@ dotenv.config();
 export const ENV = {
     PORT: process.env.PORT || 3000,
     DB_URL: process.env.DB_URL || "mongodb://localhost:27017/test",
+    NODE_ENV: process.env.NODE_ENV || "development",
 }
