a18ad5a839807b1c3f2d4892a2d4b4cfde7a063c
Prasun3003 <prasunjha3003@gmail.com>
2025-11-21 04:12:48 +0530

    problems page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,38 @@
-import {SignInButton, SignUpButton, UserButton, SignedOut, SignedIn, SignOutButton, useUser} from '@clerk/clerk-react'
-import { Routes, Route ,Navigate} from 'react-router'
-import HomePage from './pages/HomePage'
-import AboutPage from './pages/AboutPage'
-import ProblemsPage from './pages/ProblemsPage'
-import { useEffect } from 'react'
-import { Toaster } from 'react-hot-toast'
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+  useUser,
+} from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+
+import ProblemsPage from "./pages/ProblemsPage";
+import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
+import ProblemPage from "./pages/ProblemPage";
 
 function App() {
-  const  {isSignedIn}= useUser()
-  useEffect(() => {
-    console.log(isSignedIn)
-  }, [isSignedIn])
+  const { isSignedIn, isLoaded } = useUser();
+
+  // this will get rid of the flickering effect
+  if (!isLoaded) return null;
+
   return (
     <>
-    <Routes>
-     <Route path="/" element={<HomePage />} />
-     <Route path="/about" element={<AboutPage />} />
-     <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to="/" />}   />
-    </Routes>
-    <Toaster/>
+      <Routes>
+        <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
+
+        <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
+      </Routes>
+
+      <Toaster toastOptions={{ duration: 3000 }} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
