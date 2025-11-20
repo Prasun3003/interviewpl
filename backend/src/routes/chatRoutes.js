3aa47c71b810a273ed9e8b4b609d11d8071053fe
Prasun3003 <prasunjha3003@gmail.com>
2025-11-20 13:53:45 +0530

    server auth setup protect routes

---
+++ backend/src/routes/chatRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+const router = express.Router();
+
+import { protectRoute } from "../middleware/protectRoute.js";
+import {getStreamToken} from "../controllers/chatController.js"
+
+router.get("/token",protectRoute,getStreamToken)
+    
+
+export default router
