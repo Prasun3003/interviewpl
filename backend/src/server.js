cd278c6fdcafb943a3614ddbce5b9b4d4d9c8469
Prasun3003 <prasunjha3003@gmail.com>
2025-11-21 23:44:28 +0530

    dashboard page added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,102 +1,44 @@
-// import express from "express";
-// import { ENV } from "./lib/env.js";
-// import path from "path";
-// import { connectDB } from "./lib/db.js";
-// const app = express();
-// import cors from "cors";
-// import {serve} from "inngest/express"
-
-// import { inngest, functions } from "./lib/inngest.js"
-
-// const __dirname = path.resolve();
-
-// app.use(express.json());
-// app.use(cors({origin: "http://localhost:5173", credentials: true}));
-
-// app.use("/api/inngest",serve({client:inngest,functions}))
-
-// app.get("/hello", (req, res) => {
-//   res.status(200).json({ message: "Hello World" });
-// });
-
-// //make our app ready for deployment
-
-// if (ENV.NODE_ENV === "production") {
-//   const frontendPath = path.join(__dirname, "../../frontend/dist");
-
-//   app.use(express.static(frontendPath));
-
-//   app.get("/*", (req, res) => {
-//     res.sendFile(path.join(frontendPath, "index.html"));
-//   });
-// }
-
-
-// app.get("/books",(req,res)=>{
-//   res.status(200).json({ message: "Books" });
-// });
-
-// console.log(functions);
-
-
-
-// const startServer = async () => {
-//   try{
-//     await connectDB();
-//     app.listen(ENV.PORT, () => {  
-//       console.log(`Server is running on http://localhost:${ENV.PORT}`);
-//     });
-    
-//   }catch(error){
-//     console.error("error in starting server",error);
-//     process.exit(1);
-//   }
-// };
-
-// startServer();
-
 import express from "express";
+import path from "path";
 import cors from "cors";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./lib/inngest.js";
-import { connectDB } from "./lib/db.js";
+import { clerkMiddleware } from "@clerk/express";
+
 import { ENV } from "./lib/env.js";
-import sessionRoutes from "./routes/sessionRoutes.js";
+import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
 
 import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
-import {clerkMiddleware} from "@clerk/express";
-import {protectRoute} from "./middleware/protectRoute.js";
 const app = express();
 
+const __dirname = path.resolve();
+
+// middleware
 app.use(express.json());
-app.use(cors({ origin: "*"}));
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-app.use(clerkMiddleware());
-
-// Inngest endpoint
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
-// Simple health check
-
-app.use("/api/chat",chatRoutes)
-app.use("api/sessions",sessionRoutes)
-
-app.get("/", (req, res) => {
-  res.json({ status: "ok", inngest: true });
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-app.get("/video-calls",protectRoute,(req,res)=>{
-    res.status(200).json({message:"Video calls"})
-})
+// make our app ready for deployment
 
-async function start() {
-  await connectDB();
 
-  app.listen(ENV.PORT, () => {
-    console.log("Server ready on port", ENV.PORT);
-  });
-}
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
+};
 
-start();
-
+startServer();
